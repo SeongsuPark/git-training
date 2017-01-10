f84a66bc717b8e71c31bc85a630d42f659e39154
SeongsuPark <ss.park@sk.com>
2017-01-10 14:42:33 +0900

    report card: Get aavarage of grades
    Signed-off-by: SeongsuPark <ss.park@sk.com>

--- report_card.c
+++ report_card.c
@@ -1,3 +1,8 @@
 printf("%s", "abcd");
 printf("%s", "123456");
 printf("%s", "@#$@#$");
+
+printf("%s", "oudfskjfldjf");
+printf("%s", "oudfskjfldjf");
+printf("%s", "oudfskjfldjf");
+printf("%s", "oudfskjfldjf");
